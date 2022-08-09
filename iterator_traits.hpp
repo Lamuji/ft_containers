d39c2aaf70787f46714810522014d62aaa58c60c
Ramzi Fkaier <rfkaier@c3r2p3.42nice.fr>
2022-08-09 12:00:32 +0200

    first

--- iterator_traits.hpp
+++ iterator_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: rfkaier <rfkaier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 18:09:31 by rfkaier           #+#    #+#             */
-/*   Updated: 2022/08/02 19:53:57 by rfkaier          ###   ########.fr       */
+/*   Updated: 2022/08/08 14:15:52 by rfkaier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ struct iterator_traits
 {
    typedef typename Iterator::value_type value_type;
    typedef typename Iterator::difference_type
-                              difference_type;
+                              
    typedef typename Iterator::pointer pointer;
    typedef typename Iterator::reference reference;
    typedef typename Iterator::iterator_category
-                              iterator_category;
+                              
 };
 
 // Specialization
