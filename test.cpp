e735722f1ebc033692ba2e7f29d39ea7312d02d6
test <test@github.com>
2022-08-17 20:11:25 +0200

    capacity

--- test.cpp
+++ test.cpp
@@ -4,22 +4,40 @@
 #include "vector/vector.hpp"
 #include <vector>
 
-
+using namespace std;
 
 int main() 
 {
-  std::vector<int> titi;
-  titi.push_back(5);
-  titi.push_back(7);
-  titi.push_back(4);
-  titi.push_back(9);
-
-  ft::vector<int> toto(4, 100);
-  int *i = toto.data();
+  ft::vector<int> titi(10, 24);
+ // std::vector<int> toto;
+  titi.reserve(5);
+  // titi.resize(8);
+   cout << titi.capacity() << endl;
+     int *i = titi.data();
   for (int b = 0; b < 4; b++)
   {
-    i++;
     std::cout << *i << std::endl;
+    i++;
   }
+  // cout << titi.size() << endl;
+  // titi.resize(12);
+  // cout << titi.capacity() << endl;
+  // cout << titi.size() << endl;
+  // cout << toto.empty() << endl;
+
+  // titi.push_back(5);
+  // titi.push_back(7);
+  // titi.push_back(4);
+  // titi.push_back(9);
+
+  // ft::vector<int> toto(4, 100);
+  // int *i = toto.data();
+  // for (int b = 0; b < 4; b++)
+  // {
+  //   i++;
+  //   std::cout << *i << std::endl;
+  // }
+  // std::cout << toto.max_size() << std::endl;
+  // std::cout << titi.max_size() << std::endl;
   return 0;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 14:46:41 by rfkaier           #+#    #+#             */
-/*   Updated: 2022/08/16 16:19:33 by misaev           ###   ########.fr       */
+/*   Updated: 2022/08/17 20:09:27 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,13 @@ namespace ft
             
             /* CONSTRUCTORS */
             
-            vector (const allocator_type& alloc = allocator_type()) : _data(NULL), _alloc(alloc) {};
+            vector (const allocator_type& alloc = allocator_type()) : _data(NULL), _alloc(alloc), _size(0), _capacity(0) {};
             
             vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc)
             {
                 _data = _alloc.allocate(n);
+                _size = n;
+                _capacity = n;
                 for(size_type i = 0; i < n; i++)
                     _alloc.construct(_data + i, val);
             }
@@ -45,6 +47,8 @@ namespace ft
             typename ft::enable_if<!ft::is_integral<InputIterator>::value, bool>::type = false) : _alloc(alloc)
             {
                 size_t n = std::distance(first, last);
+                _size = n;
+                _capacity = n;
                 _data = _alloc.allocate(n);
                 for(size_type i = 0; i < n; i++)
                     _alloc.construct(_data + i, *first++);
@@ -54,15 +58,72 @@ namespace ft
             {
                 _data = x._data;
                 _alloc = x._alloc;
+                _size = x._size;
+                _capacity = x._capacity;
             }
-            
+
+            ~vector() { _alloc.deallocate(_data, _capacity);}
+
             /* CONSTRUCTORS */
 
             vector& operator= (const vector& x)
             {
                 _data = x._data;
                 _alloc = x._alloc;
+                _size = x._size;
+                _capacity = x._capacity;
                 return *this;
+            }
+
+            size_type size() const {return _size;}
+
+            size_type max_size() const{  return _alloc.max_size();}
+            
+            // _alloc.destroy et _alloc.construct modifie la _size
+            // _alloc.allocate et _alloc.deallocate modifie la _capacity
+            void resize (size_type n, value_type val = value_type())
+            {
+                if (n < _size){
+                    for (size_type i = n ;i < _size;i++)
+                        _alloc.destroy(_data + i);
+                    _size = n;
+                }
+                else if (n > _size){
+                    pointer data_tmp = _alloc.allocate(n);
+                    size_type i = 0;
+                    for (; i < _size;i++)
+                        _alloc.construct(data_tmp + i, _data[i]);
+                    for (; i < n; ++i)
+                        _alloc.construct(data_tmp + i, val);
+                    _alloc.deallocate(_data, _capacity);
+                    _data = _alloc.allocate(n);
+                    for (size_type t = 0; t < n; t++)
+                        _alloc.construct(_data + t, data_tmp[t]);
+                    for (size_type t = 0; t < n; t++)
+                        _alloc.destroy(data_tmp + t);
+                    _alloc.deallocate(data_tmp, n);
+                    _capacity = n;
+                    _size = n;
+                }
+            }
+
+            size_type capacity() const { return _capacity;}
+
+            bool empty() const { return _size == 0? true : false;}
+
+            void    reserve(size_type n)
+            {
+                if (n > _capacity){
+                    pointer data_tmp = _alloc.allocate(n);
+                    size_type i = 0;
+                    for (; i < n; i++)
+                        _alloc.construct(data_tmp + i, _data[i]);
+                    _alloc.deallocate(_data, _capacity);
+                    _data = _alloc.allocate(n);
+                    _data = data_tmp;
+                    _alloc.deallocate(data_tmp, n);
+                    _capacity = n;
+                }
             }
 
             /* a supprimer */
@@ -85,6 +146,8 @@ namespace ft
         private:
             allocator_type _alloc;
             pointer _data;
+            size_type _size;
+            size_type _capacity;
     };
 }
 
